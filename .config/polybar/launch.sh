a347782f466780be09aa85f26ea11cad1650cdf2
Christian Bachhelm <cbachhelm@tuta.io>
2022-07-02 21:59:54 +0200

    Wait until all processes are done in polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,9 @@
 # Nuke already running instances of polybar
 killall -q polybar
 
+# Wait until the processes have been shut down.
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 # Launch bar called cribac
 echo "---" | tee -a /tmp/polybar_cribac.log
 polybar cribac 2>&1 | tee -a /tmp/polybar_cribac.log & disown
